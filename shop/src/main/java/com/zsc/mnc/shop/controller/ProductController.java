08498368e4323f7991a880dc96281d25388342f2
linxinting <1315943577@qq.com>
2020-02-25 22:34:13 +0800

    解决时间不正确问题

--- shop/src/main/java/com/zsc/mnc/shop/controller/ProductController.java
+++ shop/src/main/java/com/zsc/mnc/shop/controller/ProductController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -22,7 +24,7 @@ public class ProductController {
     private ProductService productService;
 
     @RequestMapping(value = "/addProduct",method = RequestMethod.POST)
-    public ResponseResult addProduct(Product product){
+    public ResponseResult addProduct(Product product) throws ParseException {
         ResponseResult result = new ResponseResult();
         product.setCreateDate(new Date());
         int a=productService.addProduct(product);
